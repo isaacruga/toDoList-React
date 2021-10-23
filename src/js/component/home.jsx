a740750f3d02673b616951127ca22d0ea1eed1c8
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-10-23 14:10:53 +0000

    title.jsx, home.jsx modified

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Title from "./title.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Title />
+			<div className="input-group input-group-lg">
+				<input
+					type="text"
+					className="form-control"
+					aria-label="Sizing example input"
+					aria-describedby="inputGroup-sizing-lg"
+				/>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = () => {
+	return (
+		<h1 className="fw-lighter text-danger text-opacity-25 fs-1">todos</h1>
+	);
+};
+
+export default Title;
