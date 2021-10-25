c9bb0d6f0719e8d0b0c9fe3184b09c3cf91805b7
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-10-25 15:51:53 +0000

    todolist modified

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./title.jsx";
 
 //create your first component
 const Home = () => {
+	const [list, setList] = useState("hoa");
+
 	return (
 		<div className="text-center mt-5">
 			<Title />
 			<div className="input-group input-group-lg">
 				<input
+					onKeyUp={x => {
+						if (x.keyCode === 13) {
+							setList([x.target.value]); //...list (copia de [lis])
+						}
+					}}
 					type="text"
 					className="form-control"
 					aria-label="Sizing example input"
--- src/js/component/title.jsx
+++ src/js/component/title.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Title = () => {
+	let styleCSS = {
+		fontSize: "5rem"
+	};
+
 	return (
-		<h1 className="fw-lighter text-danger text-opacity-25 fs-1">todos</h1>
+		<h1 className="fw-lighter text-danger text-opacity-25" style={styleCSS}>
+			todos
+		</h1>
 	);
 };
 
